7eef6493eee3ba00838dd0fa5e80c8bf725b1b97
Erwan31 <spilmonterwan@gmail.com>
2020-06-28 23:23:55 +0100

    Sticky navbar

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-const nav = document.querySelector(".nav-item");
+const navItem = document.querySelector(".nav-item");
 const navLinks = document.querySelectorAll(".nav-link");
 let activeLink = undefined;
 
+/* Selection nav link active state */
 function navLink () {
-
     navLinks.forEach( link => {
-
-
         link.addEventListener('click', () => {
             link.classList.add("active");
         });
-
     });
 }
 
 navLink();
+
+
+/* Scroll detection for sticky navbar */
+window.onscroll = function (){
+    scrollStickiness();
+}
+
+const nav = document.querySelector("nav");
+let stickyOffset = nav.offsetTop;
+
+function scrollStickiness() {
+    if(window.pageYOffset >= stickyOffset){
+        nav.classList.add("sticky");
+    }
+    else{
+        navbar.classList.remove("sticky");
+    }
+}
