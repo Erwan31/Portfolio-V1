b726dda5eb041ebefa7d16a2523ef53f9b7b471b
Erwan31 <spilmonterwan@gmail.com>
2020-06-29 15:10:59 +0100

    Sticking+Hidding

--- app.js
+++ app.js
@@ -15,12 +15,18 @@ navLink();
 
 
 /* Scroll detection for sticky navbar */
+const windowH = window.innerHeight;
+
 window.onscroll = function (){
-    scrollStickiness();
+    //scrollStickiness();
+    scrollStickyOrHidden();
 }
 
 const nav = document.querySelector("nav");
+const body = document.querySelector("body");
 let stickyOffset = nav.offsetTop;
+let scrollTop = document.documentElement.scrollTop;
+let lastScrollTop = (nav.offsetHeight*2) + 'px';
 
 function scrollStickiness() {
     if(window.pageYOffset >= stickyOffset){
@@ -29,4 +35,29 @@ function scrollStickiness() {
     else{
         navbar.classList.remove("sticky");
     }
+}
+
+function scrollStickyOrHidden() {
+    /*if( scrollTop) {
+        nav.classList.add("fixed-top");
+        body.style.paddingTop = nav.offsetHeight + 'px';
+        console.log('innerH: ', nav.offsetHeight);
+    }
+    else{
+        nav.classList.remove("fixed-top");
+        body.style.paddingTop = 0;
+    }*/
+
+    let scrollH = window.pageYOffset;
+    if (scrollH > lastScrollTop){
+       // downscroll code
+       nav.classList.remove("navbar-fixed-top");
+       nav.style.paddingTop = 0;
+    } else {
+       // upscroll code
+       nav.classList.add("navbar-fixed-top");
+       nav.style.paddingTop = 10 + 'px';
+       console.log('innerH: ', nav.offsetHeight);
+    }
+    lastScrollTop = scrollH <= 0 ? 0 : scrollH; // For Mobile or negative scrolling
 }
