ff2a98b33c7fef7b713b968586198b1ce7597d29
Erwan31 <spilmonterwan@gmail.com>
2020-07-11 11:05:41 +0100

    First try for exp

---
+++ experience.js
@@ -0,0 +1,22 @@
+function openLocation ( evt , place) {
+
+    let i;
+    const tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    tablinks = document.getElementsByClassName("tablinks");
+
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(place).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
