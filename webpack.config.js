fbb6f644218f86d361e958d03ebc0f074d76ac56
Erwan31 <spilmonterwan@gmail.com>
2020-07-26 15:55:55 +0100

    Cache busting, images not working with that so far

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
+    output: {
+        filename: "main.[contentHash].js",
+        path: path.resolve(__dirname, 'dist'),
+    },
     module: {
         rules: [
             { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-            {test: /\.(png|jpg)$/, loader: 'url-loader'},
+            { test: /\.(png|jpg|gif)$/i, loader: 'url-loader'},
         ]
-    }
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/template.html"
+    })],
 };
