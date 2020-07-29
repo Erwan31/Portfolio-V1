af89ac38ce4265f4fde54b2109431af24c33e4fe
Erwan31 <spilmonterwan@gmail.com>
2020-07-29 18:49:16 +0100

    Resume button in non-responsive

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import { navAll } from "../navbar.js";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import "../css/global.css";
 import "../css/about.css";
 import "../css/burger.css";
 import "../css/contact.css";
 import "../css/experience.css";
 import "../css/getmycv.css";
-import "../css/global.css";
 import "../css/intro.css";
 import "../css/navbar.css";
 import "../css/projects.css"; //-> need an image loader for the png used in the background
